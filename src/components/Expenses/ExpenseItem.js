6433acf1667d45d3af6ab8103dc4c1e3465efe0a
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-02-05 09:51:07 +0300

    Initial commit

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -5,12 +5,14 @@ import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-
-  const [title, setTitle] = useState(props.title)
-
+  // function clickHandler() {}
+  const [title, setTitle] = useState(props.title);
+  console.log('ExpenseItem evaluated by React');
+  
   const clickHandler = () => {
-    setTitle('Updated!!')
-  }
+    setTitle('Updated!');
+    console.log(title);
+  };
 
   return (
     <Card className='expense-item'>
@@ -19,7 +21,7 @@ const ExpenseItem = (props) => {
         <h2>{title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
-      <button onClick={clickHandler}> Change title</button>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
